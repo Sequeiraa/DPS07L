8a125e8ee0bfdc6078fd0a8dc79ae5203812d3d9
Sequeiraa <sequeirajosecarlos@gmail.com>
2023-09-13 19:00:15 -0600

    Procedimiento guia 5

---
+++ Semana 7/uplifting-chip/App.js
@@ -0,0 +1,87 @@
+/*** @format
+* @flow strict-local
+*/
+import React, {useState, useEffect} from 'react';
+import colors from './src/utils/colors'
+import Form from './components/forms'
+import Footer from './components/Footers';
+import Result from './components/Result';
+import {
+ SafeAreaView,
+ StyleSheet,
+ View,
+ Text,
+ StatusBar
+} from 'react-native';
+export default function App(){
+  const[capital, setCapital] = useState(null);
+  const[interest, setInterest] = useState(null);
+  const[months,setMonths] = useState(null);
+  const [total, setTotal] = useState(null);
+ const [errorMessage, setErrorMessage] = useState('');
+ 
+ useEffect(() => {
+ if (capital && interest && months) calculate();
+  else reset();
+ }, [capital, interest, months]);
+
+const calculate = () => {
+ reset();
+ if (!capital) {
+ setErrorMessage('Añade la cantidad que quieres solicitar');
+ } else if (!interest) {
+ setErrorMessage('Añade el interes del prestamos');
+ } else if (!months) {
+ setErrorMessage('Seleccióna los meses a pagar');
+ } else {
+ const i = interest / 100;
+ const fee = capital / ((1 - Math.pow(i + 1, -months)) / i);
+ setTotal({
+ monthlyFee: fee.toFixed(2).replace('.', ','),
+ totalPayable: (fee * months).toFixed(2).replace('.', ','),
+ });
+ }
+ };
+
+  const reset = () => {
+ setErrorMessage('');
+ setTotal(null);
+ };
+
+return(
+ <>
+ <StatusBar barStyle="light-content"/>
+ <SafeAreaView style={styles.Header}>
+ <Text style={styles.HeadApp}>Cotizador de Prestamos</Text>
+ <Form
+  setCapital = {setCapital}
+  setInterest = {setInterest}
+  setMonths = {setMonths}
+ />
+ </SafeAreaView>
+ <Result
+ capital={capital}
+ interest={interest}
+ months={months}
+ total={total}
+ errorMessage={errorMessage}
+ />
+ <Footer calculate={calculate} />
+ </>
+);
+}
+const styles = StyleSheet.create({
+ Header:{
+ backgroundColor:colors.PRIMARY_COLOR,
+ height:200,
+ borderBottomLeftRadius:30,
+ borderBottomRightRadius:30,
+ alignItems:'center'
+ },
+ HeadApp:{
+   fontSize: 25,
+   fontWeight:'bold',
+   color:'#fff',
+   marginTop: 15,
+ },
+})
