0451c4467b85af077a52f59ca263b57b1c5eaac1
Sequeiraa <sequeirajosecarlos@gmail.com>
2023-11-08 19:08:22 -0600

    guia 11

---
+++ Semana 15/Birthday/src/utils/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAiHFT43XLWcwdKrl81RVxF4GqsDp8xddI",
+  authDomain: "birthday-91d44.firebaseapp.com",
+  projectId: "birthday-91d44",
+  storageBucket: "birthday-91d44.appspot.com",
+  messagingSenderId: "944674078504",
+  appId: "1:944674078504:web:82c9b318d6db48f575da1d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
